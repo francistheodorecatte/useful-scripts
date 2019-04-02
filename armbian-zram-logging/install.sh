072728ea56d9879c2649f195691256dafadc1469
Francis Theodore Catte <hellomunkey333@gmail.com>
2019-04-02 10:22:29 -0400

    Added default configs for armbian-ramlog and armbian-zram-config, updated and commented the install.sh script, and fixed perms to be root:root 755 for all files.

--- armbian-zram-logging/install.sh
+++ armbian-zram-logging/install.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
+
+# Armbian zram and log2zram installer script
+# Requires Debian Jessie or newer
+# By Franics Theodore Catte, 2019.
+
+# copy armbian scripts
 mkdir /usr/lib/armbian
 cp ./armbian-ramlog /usr/lib/armbian
 cp ./armbian-zram-config /usr/lib/armbian
 cp ./armbian-truncate-logs /usr/lib/armbian
+
+# copy default configs
+cp ./armbian-zram-config.dpkg-dist /etc/default
+cp ./armbian-ramlog.dpkg-dist /etc/default
+
+# setup cronjobs
 cp ./armbian-log-truncate /etc/cron.d/
 cp ./armbian-ram-logging /etc/cron.daily/
+systemctl restart cron
+
+# setup systemd services
 cp ./armbian*.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable armbian-ramlog
 systemctl enable armbian-zram-config
-echo "WARNING!!! Going down for reboot!"
-reboot
+systemctl start armbian-ramlog
+systemctl start armbian-zram-config
 
