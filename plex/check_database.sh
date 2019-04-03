6c1f50232996b8e0f19048eee304e148a40e8c1e
Francis Theodore Catte hellomunkey333@gmail.com <hellomunkey333@gmail.com>
2019-04-02 20:40:45 -0400

    Adding plexmediaserver database repair script.

---
+++ plex/check_database.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PLEX_HOME="/srv/dev-disk-by-label-PLEXSTORE/plexmediaserver/"
+
+apt update
+apt -y install sqlite3
+
+cd $PLEX_HOME/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/
+
+echo "Stopping plexmediaserver..."
+systemctl stop plexmediaserver
+
+echo "Checking for DB Corruption..."
+cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
+sqlite3 com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
+sqlite3 com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
+sqlite3 com.plexapp.plugins.library.db "PRAGMA integrity_check"
+
+echo "Running repairs..."
+cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
+sqlite3 com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
+sqlite3 com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
+sqlite3 com.plexapp.plugins.library.db .dump > dump.sql
+rm com.plexapp.plugins.library.db
+sqlite3 com.plexapp.plugins.library.db < dump.sql
+
+echo "Cleaning up..."
+rm dump.sql
+mv com.plexapp.plugins.library.db-shm com.plexapp.plugins.library.db-shm.original
+mv com.plexapp.plugins.library.db-wal com.plexapp.plugins.library.db-wal.original
+
+echo "Starting plexmediaserver..."
+systemctl restart plexmediaserver
+
+echo "Creating cron.daily script to restart plexmediaserver..."
+echo "#!/bin/sh" > /etc/cron.daily/plexmediaserver
+echo "systemctl restart plexmediaserver" >> /etc/cron.daily/plexmediaserver
+chmod 755 /etc/cron.daily/plexmediaserver
+chmod u+x /etc/cron.daily/plexmediaserver
