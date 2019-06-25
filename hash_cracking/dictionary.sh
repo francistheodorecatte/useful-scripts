41a40e6d8bb5763b3f1601fc7e44d62848c8bce9
phineasthecat [hellomunkey333@gmail.com] <hellomunkey333@gmail.com>
2019-06-25 12:55:45 +0000

    Added simple script to pull down multiple different word and password lists, then combine them into one dictionary.

---
+++ hash_cracking/dictionary.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# a simple script to put together a basic dictionary of words and passwords.
+# basically just to save a lot of typing.
+# this pulls from lists provided by skull security.
+# once downloaded, they're unpacked, and shuffled together into a single file.
+# here, it's just dictionary.txt in the root of your home folder.
+
+mkdir ~/wordlists
+cd ~/wordlists
+wget http://downloads.skullsecurity.org/passwords/john.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/cain.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/conficker.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/500-worst-passwords.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/twitter-banned.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/phpbb.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/myspace.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/hotmail.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/faithwriters.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/elitehacker.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/hak5.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/facebook-pastebay.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/facebook-phished.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/carders.cc.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/english.txt.bz2
+bunzip2 *.bz2
+cat *.txt >> ~/dictionary.txt
