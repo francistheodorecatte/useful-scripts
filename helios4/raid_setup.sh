20eacdf22882e4eee0a78459067219508a3d53ad
phineasthecat <hellomunkey333@gmail.com>
2019-12-01 14:30:29 -0500

    fixed badblocks check to only run a short SMART test, and wait for the results before continuing

--- helios4/raid_setup.sh
+++ helios4/raid_setup.sh
@@ -54,7 +54,8 @@ echo -e "Running disk checks.\nThis may take a very long time!" 2>&1 | tee $LOG
 for Dev in /sys/block/sd* ; do
 	echo -e "Checking /dev/${Dev##*/} for bad blocks..." 2>&1 | tee $LOG
         badblocks -sv -b 4096 -t 0x00 -o ./badblocks_${Dev##*/}.txt /dev/${Dev##*/} 2>&1 | tee $LOG \
-        && smartctl -t long -C /dev/${Dev##*/} 2>&1 | tee $LOG \
+        && smartctl -t short -C /dev/${Dev##*/} 2>&1 | tee $LOG \
+	&& sleep 121 \ #wait two minutes for test results to show in disk's SMART log
         && smartctl -H /dev/${Dev##*/} 2>&1 | tee $LOG \
         && smartctl -l selftest /dev/${Dev##*/} 2>&1 | tee $LOG \
         && sleep 2
